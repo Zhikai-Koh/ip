38437d3e9fad40faccd51e0680195715baf860c7
Koh Zhi Kai <zhikai2004@gmail.com>
2026-08-21 13:48:45 +0800

    Completed level 1 - Echo

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Starts the Bob chatbot application.
  */
@@ -8,15 +10,31 @@ public class Bob {
      * @param args command-line arguments, which are not used
      */
     public static void main(String[] args) {
-        String seperator = "____________________________________________________________";
+        String separator = "____________________________________________________________";
         String banner = " ____        _     \n"
                 + "| __ )  ___ | |__  \n"
                 + "|  _ \\ / _ \\| '_ \\ \n"
                 + "| |_) | (_) | |_) |\n"
                 + "|____/ \\___/|_.__/ \n";
-        String greetings = "Hello! I'm Bob.\n" +
-                "What can I do for you?\n";
+        String greetings = "Hello! I'm Bob.\n"
+                + "What can I do for you?\n";
         String goodbyes = "Bye. Hope to see you again soon!\n";
-        System.out.println(seperator + "\n" + banner +  greetings + seperator + "\n" + goodbyes + seperator);
+
+        System.out.println(separator + "\n" + banner + greetings + separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.print(goodbyes);
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
